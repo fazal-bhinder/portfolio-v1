026ce6da6380c30f8988f4d0a0cf8ed479dcdec1
fazal-bhinder <163873947+fazal-bhinder@users.noreply.github.com>
2025-07-13 19:39:55 +0530

    minor changes

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -4,6 +4,8 @@ import MediumImg from "@/public/projects/medium.jpeg"
 import PaytmImg from "@/public/projects/paytm.jpeg";
 import F1 from "@/public/projects/f1.jpeg";
 import GYM from "@/public/projects/gym.jpeg";
+import Resume from "@/public/projects/resume.jpeg";
+import Todo from "@/public/projects/todo.jpeg";
 
 
 export const projects = [
@@ -28,6 +30,13 @@ export const projects = [
     liveUrl: "https://racing-f1.b-cdn.net/",
     githubUrl: "https://github.com/fazal-bhinder/formula-1",
   },
+    {
+    title: "Old Portfolio",
+    description: "This is my old portfolio website, showcasing my skills and projects as a full stack developer. It features a clean design, responsive layout, and highlights my work.",
+    imageSrc: Resume,
+    liveUrl: "https://portfolio-plum-mu-29.vercel.app",
+    githubUrl: "https://github.com/fazal-bhinder/portfolio",
+  }, 
   {
     title: "PayTm clone",
     description: "Built a full-stack Paytm-like payment app. Enabling users to transfer funds between bank and wallet, and send money to friends. Separate payment gateway routing, and real-time transaction of magic, and a lot of love.",
@@ -42,6 +51,13 @@ export const projects = [
     liveUrl: "https://github.com/fazal-bhinder/muzic",
     githubUrl: "https://github.com/fazal-bhinder/muzic",
   },
+  {
+    title: "Todo App",
+    description: "This is a simple todo app built with React and Next.js. It showcases my skills in building user interfaces and managing state.",
+    imageSrc: Todo,
+    liveUrl: "https://todo-frontend-flame-psi.vercel.app",
+    githubUrl: "https://github.com/fazal-bhinder/todo_app",
+  }, 
   {
     title: "GYM",
     description: "Built a responsive gym website with a modern design. It features a clean layout, smooth animations, and a user-friendly interface.",
