60c0a46d26b6f29fc823f379caa4274dc64ab4aa
fazal-bhinder <163873947+fazal-bhinder@users.noreply.github.com>
2025-08-07 02:23:46 +0530

    first commit

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,11 +1,11 @@
-import TrashAI from "/public/projects/trash.jpeg";
+import TrashAI from "/public/projects/TrashAI.jpeg";
 import MuzicImg from "@/public/projects/muzic.jpeg";
 import MediumImg from "@/public/projects/medium.jpeg"
 import PaytmImg from "@/public/projects/paytm.jpeg";
 import F1 from "@/public/projects/f1.jpeg";
 import GYM from "@/public/projects/gym.jpeg";
 import Resume from "@/public/projects/resume.jpeg";
-import Todo from "@/public/projects/todo.jpeg";
+import CryptoPunks from "@/public/projects/CryptoPunks.jpeg";
 
 
 export const projects = [
@@ -17,18 +17,18 @@ export const projects = [
     githubUrl: "https://github.com/fazal-bhinder/TrashAI",
   },
   {
+    title: "CryptoPunks",
+    description: "A modern, responsive web application showcasing CryptoPunks NFT marketplace built with Next.js, React, TypeScript, and Tailwind CSS.",
+    imageSrc: CryptoPunks,
+    liveUrl: "https://crypto-punks-sandy.vercel.app/",
+    githubUrl: "https://github.com/fazal-bhinder/CryptoPunks",
+  },
+  {
     title: "Medium Clone",
     description: "Created a Medium-style blogging platform Implemented authentication, CRUD operations, and a clean editor interface.",
     imageSrc: MediumImg,
     liveUrl: "https://medium-frontend-beta.vercel.app/",
     githubUrl: "https://github.com/fazal-bhinder/medium?tab=readme-ov-file",
-  },
-  {
-    title: "Muzic",
-    description: "Developed a Spotify-like SaaS app allowing users to host and join live music streams. Features include collaborative song queues with upvote/downvote functionality, and a clean interface.",
-    imageSrc: MuzicImg,
-    liveUrl: "https://github.com/fazal-bhinder/muzic",
-    githubUrl: "https://github.com/fazal-bhinder/muzic",
   },
   {
     title: "Old Portfolio",
@@ -38,13 +38,6 @@ export const projects = [
     githubUrl: "https://github.com/fazal-bhinder/portfolio",
   }, 
   {
-    title: "PayTm clone",
-    description: "Built a full-stack Paytm-like payment app. Enabling users to transfer funds between bank and wallet, and send money to friends. Separate payment gateway routing, and real-time transaction of magic, and a lot of love.",
-    imageSrc: PaytmImg,
-    liveUrl: "https://github.com/fazal-bhinder/paytm?tab=readme-ov-file",
-    githubUrl: "https://github.com/fazal-bhinder/paytm?tab=readme-ov-file",
-  },
-  {
     title: "Formla 1",
     description: "Inspired by the thrill and intensity of Formula 1. It features a dynamic fullscreen video background, iconic quotes from drivers, and a sleek, responsive design.",
     imageSrc: F1,
@@ -52,12 +45,19 @@ export const projects = [
     githubUrl: "https://github.com/fazal-bhinder/formula-1",
   },
   {
-    title: "Todo App",
-    description: "This is a simple todo app built with React and Next.js. It showcases my skills in building user interfaces and managing state.",
-    imageSrc: Todo,
-    liveUrl: "https://todo-frontend-flame-psi.vercel.app",
-    githubUrl: "https://github.com/fazal-bhinder/todo_app",
-  }, 
+    title: "PayTm clone",
+    description: "Built a full-stack Paytm-like payment app. Enabling users to transfer funds between bank and wallet, and send money to friends. Separate payment gateway routing, and real-time transaction of magic, and a lot of love.",
+    imageSrc: PaytmImg,
+    liveUrl: "https://github.com/fazal-bhinder/paytm?tab=readme-ov-file",
+    githubUrl: "https://github.com/fazal-bhinder/paytm?tab=readme-ov-file",
+  },
+  {
+    title: "Muzic",
+    description: "Developed a Spotify-like SaaS app allowing users to host and join live music streams. Features include collaborative song queues with upvote/downvote functionality, and a clean interface.",
+    imageSrc: MuzicImg,
+    liveUrl: "https://github.com/fazal-bhinder/muzic",
+    githubUrl: "https://github.com/fazal-bhinder/muzic",
+  },
   {
     title: "GYM",
     description: "Built a responsive gym website with a modern design. It features a clean layout, smooth animations, and a user-friendly interface.",
